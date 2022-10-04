2be0db38ad64e54bebd6cfc96ec24ef8fa4ce8e4
Miguel Ángel Barquilla <miguelangelbarquilla@gmail.com>
2022-10-04 19:15:50 +0200

    Modularitation JS

---
+++ public/isSameValue.js
@@ -0,0 +1,5 @@
+const isSameValue = (value1, value2) => {
+  return Object.is(value1, value1);
+};
+
+export default isSameValue;
---
+++ public/main.js
@@ -0,0 +1,3 @@
+import isSameValue from "./isSameValue.js";
+
+console.log(isSameValue(0, 0));
